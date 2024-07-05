a88ff165d0f856d581bd1489e3b3a3b615f17fda
Aravind <aravindsiva2003.in@gmail.com>
2024-07-05 08:42:30 +0530

    1234567890

--- src/app/contactpage/contactpage.component.ts
+++ src/app/contactpage/contactpage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-contactpage',
   standalone: true,
   templateUrl: './contactpage.component.html',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HeaderComponent],
 })
 export class ContactpageComponent {}
