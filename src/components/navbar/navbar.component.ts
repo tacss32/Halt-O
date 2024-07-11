e67e137706304609e402660856cca440a64b09aa
Aravind <aravindsiva2003.in@gmail.com>
2024-07-11 06:27:29 +0530

    headers, nav, minor changes

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,8 +1,16 @@
-import { Component, HostListener, inject, signal } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  inject,
+  signal,
+  ElementRef,
+  Renderer2,
+  OnDestroy,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { NgClass } from '@angular/common';
 
-interface menu {
+interface Menu {
   name: string;
   path: string;
 }
@@ -13,39 +21,45 @@ interface menu {
   imports: [NgClass],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy {
   menuOpen = signal<boolean>(false);
   isScroll = signal<boolean>(true);
-
-  pages = signal<menu[]>([
-    {
-      name: 'Home',
-      path: '/',
-    },
-    {
-      name: 'About',
-      path: '/about',
-    },
-    {
-      name: 'Application',
-      path: '/application',
-    },
-    {
-      name: 'Parking-Ecosystem',
-      path: '/parking',
-    },
-    {
-      name: 'Contact',
-      path: '/contact',
-    },
+  pages = signal<Menu[]>([
+    { name: 'Home', path: '/' },
+    { name: 'About', path: '/about' },
+    { name: 'Application', path: '/application' },
+    { name: 'Parking-Ecosystem', path: '/parking' },
+    { name: 'Contact', path: '/contact' },
   ]);
 
   private router = inject(Router);
+  private elementRef = inject(ElementRef);
+  private renderer = inject(Renderer2);
+  private clickListener: () => void;
+  private lastScrollTop = 0;
+  navbarVisible = signal<boolean>(true);
+
+  constructor() {
+    this.clickListener = this.renderer.listen(
+      'document',
+      'click',
+      (event: Event) => {
+        this.onClickOutside(event);
+      }
+    );
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
+    const st = window.scrollY;
+    if (st > this.lastScrollTop) {
+      this.navbarVisible.set(false);
+    } else {
+      this.navbarVisible.set(true);
+    }
+    this.lastScrollTop = st <= 0 ? 0 : st;
+    this.isScroll.set(window.scrollY <= 10);
     this.menuOpen.set(false);
-    this.isScroll.set(window.scrollY <= 100);
   }
 
   toggleMenu(): void {
@@ -54,5 +68,15 @@ export class NavbarComponent {
 
   isActive(path: string): boolean {
     return this.router.url === path;
+  }
+
+  onClickOutside(event: Event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.menuOpen.set(false);
+    }
+  }
+
+  ngOnDestroy() {
+    this.clickListener(); // Remove the event listener when the component is destroyed
   }
 }
