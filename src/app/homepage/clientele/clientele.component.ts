961de231400cfe4b470b41c1f1c62d3ce63f8ed6
tacss32 <tacss.co@google.com>
2025-01-30 19:58:17 +0530

    clientele

---
+++ src/app/homepage/clientele/clientele.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clientele',
+  standalone: true,
+  imports: [],
+  templateUrl: './clientele.component.html'
+})
+export class ClienteleComponent {
+  logos = [
+    '../../../assets/clientele/apollo.jpg',
+    '../../../assets/clientele/MyTvsLogo.svg',
+    '../../../assets/clientele/khalf.jpg',
+    '../../../assets/clientele/urbaser.jpg',
+    '../../../assets/clientele/rootquotient.png',
+    '../../../assets/clientele/hyundai.png',
+    '../../../assets/clientele/upavihar.jpg',
+    '../../../assets/clientele/v3.jpg',
+    '../../../assets/clientele/cumta.png',
+    '../../../assets/clientele/kyn.jpg',
+  ];
+
+  // Duplicate logos for infinite scrolling effect
+  duplicatedLogos = [...this.logos, ...this.logos, ...this.logos];
+}
